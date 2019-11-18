9c41d75107408ba6ecc1e6fd3ac0ec6e61acdb94
Maurizio Conti <posta@maurizioconti.com>
2019-11-18 19:55:56 +0100

    Primo commit

---
+++ EsercizioNumeri.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class EsercizioNumeri
+{
+    public static string AggiungeUno(int val)
+    {
+        return (val+1).ToString();
+    }
+}
---
+++ EsercizioNumeriTest.cs
@@ -0,0 +1,12 @@
+// This file was auto-generated based on version 1.2.0 of the canonical data.
+
+using Xunit;
+
+public class EsercizioNumeriTest
+{
+    [Fact]
+    public void AggiungeUno()
+    {
+        Assert.Equal("2", EsercizioNumeri.AggiungeUno(1));
+    }
+}
