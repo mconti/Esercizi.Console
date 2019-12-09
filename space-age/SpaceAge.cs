83da7a2573fadd4eee3575ab0714f3548f6a3a80
Maurizio Conti <posta@maurizioconti.com>
2019-12-09 20:52:32 +0100

    EtaSpaziale

---
+++ space-age/SpaceAge.cs
@@ -0,0 +1,42 @@
+using System;
+
+public class EtaSpaziale
+{
+    public EtaSpaziale(int seconds)
+    {
+    }
+
+    public double OnEarth()
+    {
+    }
+
+    public double OnMercury()
+    {
+    }
+
+    public double OnVenus()
+    {
+    }
+
+    public double OnMars()
+    {
+
+    }
+
+    public double OnJupiter()
+    {
+
+    }
+
+    public double OnSaturn()
+    {
+    }
+
+    public double OnUranus()
+    {
+    }
+
+    public double OnNeptune()
+    {
+    }
+}
