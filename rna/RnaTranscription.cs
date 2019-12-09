e38f1b14b457ec75f2889956c87534a161c8fd35
Maurizio Conti <posta@maurizioconti.com>
2019-12-09 19:03:41 +0100

    rna

---
+++ rna/RnaTranscription.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+public static class TrascrizioneRNA
+{
+    public static string ToRna(string dna)
+    {
+        if( String.IsNullOrEmpty( dna ) )
+            return "";
+
+        return dna;
+    }
+}
