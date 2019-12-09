76a20a92f2d4ff125e558165847be7fad5d79931
Maurizio Conti <posta@maurizioconti.com>
2019-12-09 19:46:45 +0100

    Media senza istruzioni

--- mediaPesata/mediaPesata.cs
+++ mediaPesata/mediaPesata.cs
@@ -5,23 +5,10 @@ public static class MediaPesata
 {
     public static double Calcola(string nomeFile)
     {
-        // Apre il file richiesto
-        StreamReader  sr = new StreamReader( nomeFile );
-        
-        // Legge solo la prima riga
-        string riga = sr.ReadLine();
-
-        // scompone in due colonne la riga
-        string[] colonne = riga.Split(';');
-
-        // Trasforma le stringhe lette in valori double
-        Double.TryParse( colonne[0], out double valore );
-        Double.TryParse( colonne[1], out double peso );
-
-        // esegue un calcolo (giusto per fare un esempio)
-        double retVal = valore*peso;
-
-        // arrotonda a 3 cifre decimali
+        // Attenzione!!
+        // dopo aver calcolato il risultato,
+        // ritornarlo arrotondato a 3 cifre decimali
+        double retVal = 6.9619876;
         return Math.Round( retVal, 3 );
     }
 }
---
+++ mediaPesata_i/mediaPesata.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+public static class MediaPesata
+{
+    public static double Calcola(string nomeFile)
+    {
+        // Apre il file richiesto
+        StreamReader  sr = new StreamReader( nomeFile );
+        
+        // Legge solo la prima riga
+        string riga = sr.ReadLine();
+
+        // scompone in due colonne la riga
+        string[] colonne = riga.Split(';');
+
+        // Trasforma le stringhe lette in valori double
+        Double.TryParse( colonne[0], out double valore );
+        Double.TryParse( colonne[1], out double peso );
+
+        // esegue un calcolo (giusto per fare un esempio)
+        double retVal = valore*peso;
+
+        // arrotonda a 3 cifre decimali
+        return Math.Round( retVal, 3 );
+    }
+}
