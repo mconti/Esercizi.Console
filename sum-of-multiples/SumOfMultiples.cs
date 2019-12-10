a0f35d2c4e687b67acf0438089b9fa967ebc4efd
Maurizio Conti <posta@maurizioconti.com>
2019-12-10 08:40:20 +0100

    Somma di multipli

---
+++ sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class SumOfMultiples
+{
+    public static int Sum(IEnumerable<int> multiples, int max)
+    {
+        return 0;
+    }
+}
