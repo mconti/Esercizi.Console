8cf94f320d113d10978d2060e9ba5f2a0abdc7ab
Maurizio Conti <posta@maurizioconti.com>
2019-12-17 12:23:10 +0100

    .

---
+++ series/Series.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+public static class Series
+{
+    public static string[] Slices(string numbers, int sliceLength)
+    {
+        List<string> retVal = new List<string>();
+        return retVal.ToArray();
+    }
+}
