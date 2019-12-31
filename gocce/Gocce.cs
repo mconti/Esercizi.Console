359a84aa80402c30521f01fdd490dd5fe8f1d87a
Maurizio Conti <posta@maurizioconti.com>
2019-12-31 08:27:57 +0100

    .

---
+++ gocce/Gocce.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public static class Raindrops
+{
+    public static string Convert(int number)
+    {
+        string retVal="";
+        return retVal;
+    }
+}
