924a17b8a812c40bd767fd225823e4e9cfeaddca
Maurizio Conti <posta@maurizioconti.com>
2019-12-31 09:21:48 +0100

    .

---
+++ punteggio/Punteggio.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class HighScores
+{
+    public HighScores( int[] list )
+    {
+        // Costruttore...
+    }
+
+    public int[] Scores()
+    { return new int[]{}; }
+
+    public int Latest()
+    { return 0; }
+
+    public int PersonalBest()
+    { return 0; }
+
+    public int[] PersonalTopThree()
+    {
+        return new int[]{};
+    }
+}
