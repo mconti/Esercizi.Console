c811346c0ea76fa57b5f62e423d325f397e1b292
Maurizio Conti <posta@maurizioconti.com>
2020-01-07 14:12:14 +0100

    SerieProdotto

---
+++ SerieProdotto/SerieProdotto.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class LargestSeriesProduct
+{
+    public static long GetLargestProduct(string digits, int span) 
+    {
+        if( span==0 )
+            return 1;
+
+        return 0;
+    }
+}
