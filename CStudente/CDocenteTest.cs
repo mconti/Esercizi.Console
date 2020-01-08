2d7fe3902990e8973658db56d2d06ee0164df10e
Maurizio Conti <posta@maurizioconti.com>
2020-01-08 10:02:23 +0100

    CDocente

---
+++ CStudente/CDocenteTest.cs
@@ -0,0 +1,25 @@
+using System;
+using Xunit;
+
+namespace CStudente
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void test_costruisce_docente_default()
+        {
+            Docente s = new Docente();
+            Assert.Equal<string>( "-", s.Nome );
+            Assert.Equal<string>( "-", s.Cognome );
+        }
+
+        [Fact]
+        public void test_costruisce_docente_standard()
+        {
+            Docente s = new Docente( "Marco", "Pini" );
+            Assert.Equal<string>( "Marco", s.Nome );
+            Assert.Equal<string>( "Pini", s.Cognome );
+            Assert.Equal<string>( "Marco - Pini", s.ToString() );
+        }
+    }
+}
