66b4b3e476f53b575e552fb798f8fc06a28a5949
mconti <postamaurizioconti.com>
2021-12-07 10:14:58 +0100

    Nuovi test per numerichar

--- numerichar/numericharTest.cs
+++ numerichar/numericharTest.cs
@@ -10,6 +10,16 @@ public class numericharTest
         Assert.Equal(1, numerichar.Verifica(1234, 0));
     }
     [Fact]
+    public void primo_digit3()
+    {
+        Assert.Equal(4, numerichar.Verifica(123456789, 3));
+    }
+    [Fact]
+    public void primo_digit4()
+    {
+        Assert.Equal(7, numerichar.Verifica(10456789, 5));
+    }
+    [Fact]
     public void ultimo_digit1()
     {
         Assert.Equal(4, numerichar.Verifica(1234, 3));
@@ -17,12 +27,12 @@ public class numericharTest
     [Fact]
     public void primo_digit2()
     {
-        Assert.Equal(1, numerichar.Verifica(-1234, 0));
+        Assert.Equal(3, numerichar.Verifica(-1234, 2));
     }
     [Fact]
     public void ultimo_digit2()
     {
-        Assert.Equal(4, numerichar.Verifica(-1234, 3));
+        Assert.Equal(0, numerichar.Verifica(-12034, 2));
     }
     [Fact]
     public void digit_troppo_grande()
