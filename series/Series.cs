832601a22f2fc949690ef649a88f81e06517b6aa
Maurizio Conti <mconti@users.noreply.github.com>
2022-12-01 11:42:01 +0100

    Update Series.cs

--- series/Series.cs
+++ series/Series.cs
@@ -5,6 +5,9 @@ public static class Series
 {
     public static string[] Slices(string numbers, int sliceLength)
     {
+        // in caso di errore eseguire questa riga di codice per comuncare l'errore al chiamante!!
+        // throw new ArgumentException();
+        
         List<string> retVal = new List<string>();
         return retVal.ToArray();
     }
