7cb19c135503461a52ac38507df8ba2a4ef89413
Maurizio Conti <mconti@users.noreply.github.com>
2023-01-20 10:04:37 +0100

    Aggiornato test su Elimina Elemento

--- CVettore/CVettoreTest.cs
+++ CVettore/CVettoreTest.cs
@@ -56,5 +56,30 @@ namespace CVettore
             Assert.False( v.Aggiungi(51) );
             Assert.Equal<int>( 0, v.Somma() );
         }
+        
+        [Fact]
+        public void test_togli_elemento()
+        {
+            Vettore v = new Vettore();
+            Assert.False( v.EliminaUltimo() );
+
+            Assert.True( v.Aggiungi(521) );
+            Assert.Equal<int>( 521, v.Ultimo() );
+            Assert.True( v.EliminaUltimo() );
+
+            v = new Vettore( 1000 );
+            Assert.False( v.EliminaUltimo() );
+
+            for(int idx=0; idx < 1000 ; idx++ )
+            {
+                v.Aggiungi( idx );
+            }
+
+            Assert.Equal<int>( 999, v.Ultimo() );
+            Assert.Equal<int>( 499500, v.Somma() );
+
+            Assert.True( v.EliminaUltimo() );
+        }
+
     }
 }
